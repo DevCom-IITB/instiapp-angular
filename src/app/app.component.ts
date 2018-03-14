c93b0965dd5fc197361bffbbe5e50d13cfc14a1b
Varun Patil <radialapps@gmail.com>
2018-03-14 20:18:45 +0530

    Improve animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { ChangeDetectorRef, Component, Injectable, OnDestroy } from '@angular/core';
+import { ChangeDetectorRef, Component, Injectable, OnDestroy, OnInit } from '@angular/core';
 import { MediaMatcher } from '@angular/cdk/layout';
 import { Title } from '@angular/platform-browser';
 import { DataService } from './data.service';
 import { routerTransition } from './animations';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -10,7 +11,7 @@ import { routerTransition } from './animations';
   styleUrls: ['./app.component.css'],
   animations: [ routerTransition ],
 })
-export class AppComponent implements OnDestroy {
+export class AppComponent implements OnDestroy, OnInit {
   mobileQuery: MediaQueryList;
     public initialized = false;
 
@@ -21,12 +22,20 @@ export class AppComponent implements OnDestroy {
         media: MediaMatcher,
         private titleService: Title,
         public dataService: DataService,
+        public router: Router,
       ) {
 
         this.mobileQuery = media.matchMedia('(max-width: 600px)');
         this._mobileQueryListener = () => changeDetectorRef.detectChanges();
         this.mobileQuery.addListener(this._mobileQueryListener);
         this.titleService.setTitle('Home');
+    }
+
+    ngOnInit() {
+    }
+
+    scrollToTop() {
+      window.scrollTo(0, 0);
     }
 
     ngOnDestroy(): void {
