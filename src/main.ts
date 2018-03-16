3ee3ef3413c7890070150ab0f863881f4f03e999
Varun Patil <radialapps@gmail.com>
2018-03-16 22:15:05 +0530

    Add in HMR

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,18 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import { hmrBootstrap } from './hmr';
+
 if (environment.production) {
   enableProdMode();
-}
-
-platformBrowserDynamic().bootstrapModule(AppModule)
+  platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
+} else {
+  const bootstrap = () => platformBrowserDynamic().bootstrapModule(AppModule);
+  if (module[ 'hot' ]) {
+    hmrBootstrap(module, bootstrap);
+  } else {
+    console.error('HMR is not enabled for webpack-dev-server!');
+    console.log('Are you using the --hmr flag for ng serve?');
+  }
+}
