23630a6a2e50f2bbc270291126c1fdd52d253dd5
Varun Patil <radialapps@gmail.com>
2018-03-18 13:15:31 +0530

    Add login url

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -12,4 +12,20 @@ export class LoginComponent implements OnInit {
   ngOnInit() {
   }
 
+  /** Open a new tab for SSO login */
+  login() {
+    const HOST = 'https://gymkhana.iitb.ac.in/sso/oauth/authorize/';
+    const CLIENT_ID = 'vR1pU7wXWyve1rUkg0fMS6StL1Kr6paoSmRIiLXJ';
+    const RESPONSE_TYPE = 'code';
+    const SCOPE = 'basic profile picture sex ldap phone insti_address program secondary_emails';
+    const REDIR = 'http://localhost:8000/api/login';
+
+    const URL = HOST + '?client_id=' + CLIENT_ID +
+                       '&response_type=' + RESPONSE_TYPE +
+                       '&scope=' + SCOPE +
+                       '&redirect_uri=' + REDIR;
+
+    window.open(URL, '_blank');
+  }
+
 }
