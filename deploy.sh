37442301609673437546c94bd117ff81bfdb15bf
Varun Patil <radialapps@gmail.com>
2018-03-25 20:08:42 +0530

    Fix Continuous Deployment

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+if [ "$CIRCLE_BRANCH" = "master" ]; then
+
+git config --global user.name "Xunk-Bot"
+git config --global user.email "xunk@radialapps.com"
+git config --global push.default matching
+git clone git@github.com:go-xunk/iitb-app-deploy.git deploy
+
+cd deploy
+rm -r *
+cp -R ../dist/* ./ 
+date > BUILD_TIME
+git add -A
+git commit -m "Automated Build"
+git push
+cd ..
+
+else
+
+echo "Not on master branch"
+
+fi
