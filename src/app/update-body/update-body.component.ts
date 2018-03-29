f3b687ac6c07920718087b58999426c05414038f
Varun Patil <radialapps@gmail.com>
2018-03-29 12:13:44 +0530

    Add non-functional layout to update body

--- src/app/update-body/update-body.component.ts
+++ src/app/update-body/update-body.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { IBody } from '../interfaces';
+import { DataService } from '../data.service';
+import { ActivatedRoute, Params } from '@angular/router';
+import { API } from '../../api';
 
 @Component({
   selector: 'app-update-body',
@@ -7,9 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdateBodyComponent implements OnInit {
 
-  constructor() { }
+  body: IBody;
+  bodyId: string;
+
+  constructor(
+    public dataService: DataService,
+    public activatedRoute: ActivatedRoute,
+  ) { }
 
   ngOnInit() {
+    if (!this.dataService.loggedIn) {
+      alert('Please login to continue!');
+      this.dataService.navigateBack();
+      return;
+    }
+
+    this.activatedRoute.params.subscribe((params: Params) => {
+      this.bodyId = params['id'];
+      this.refresh();
+    });
+  }
+
+  /** Loads the data */
+  refresh() {
+    this.dataService.FireGET(API.Body, {uuid: this.bodyId}).subscribe(result => {
+      this.body = result as IBody;
+    });
   }
 
 }
