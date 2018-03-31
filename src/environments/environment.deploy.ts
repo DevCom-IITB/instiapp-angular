1c0cf002cf85cc790783e00c58c23644b7df9726
Varun Patil <radialapps@gmail.com>
2018-03-31 18:59:03 +0530

    Update deploy client id

--- src/environments/environment.deploy.ts
+++ src/environments/environment.deploy.ts
@@ -3,5 +3,5 @@ export const environment = {
     hmr: false,
     host: 'https://instiapp.wncc-iitb.org/',
     sso_host: 'https://gymkhana.iitb.ac.in/sso/oauth/authorize/',
-    sso_client_id: 'engewAxqT3qxNbmXAJJPbyNPyxfqyB1IOcuCIAyC',
+    sso_client_id: 'u1eFlmRWUOY5Sh4yFeqeWD71OXR8M8PYc0xUK33D',
 };
