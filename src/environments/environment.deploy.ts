c9988542a5c23580dc31c3a0f164b461f992b167
Varun Patil <radialapps@gmail.com>
2018-03-31 17:53:15 +0530

    Add deploy script

---
+++ src/environments/environment.deploy.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: true,
+    hmr: false,
+    host: 'https://instiapp.wncc-iitb.org/',
+    sso_host: 'https://gymkhana.iitb.ac.in/sso/oauth/authorize/',
+    sso_client_id: 'engewAxqT3qxNbmXAJJPbyNPyxfqyB1IOcuCIAyC',
+};
