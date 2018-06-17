110483024236a733b1c09a8810b78e4589e245bb
Varun Patil <radialapps@gmail.com>
2018-06-17 16:26:35 +0530

    DANGEROUS: Allow search-as-you-type

--- src/explore/explore.component.ts
+++ src/explore/explore.component.ts
@@ -28,15 +28,20 @@ export class ExploreComponent implements OnInit {
   }
 
   /** Fire the search API */
-  search(query: string) {
+  search(query: string, noerror: boolean = false) {
+    if (query.length < 5) {
+      if (!noerror) {
+        this.snackBar.open('Invalid query - At least 5 characters', 'Dismiss', {
+          duration: 2000,
+        });
+      }
+      return;
+    }
+
     this.dataService.FireGET<any>(API.Search, {query: query}).subscribe(result => {
       this.bodies = result.bodies;
       this.events = result.events;
       this.users = result.users;
-    }, () => {
-      this.snackBar.open('Invalid query - At least 5 characters', 'Dismiss', {
-        duration: 2000,
-      });
     });
   }
 
