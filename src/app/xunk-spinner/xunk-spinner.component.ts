bf573b28a7c67a8f27df995ea838fc6a0b16931a
Varun Patil <radialapps@gmail.com>
2018-06-27 03:31:05 +0530

    Add more status codes to error

--- src/app/xunk-spinner/xunk-spinner.component.ts
+++ src/app/xunk-spinner/xunk-spinner.component.ts
@@ -14,8 +14,12 @@ export class XunkSpinnerComponent {
   errors = {
     204: { img: this.SAD_CLOUD, m: 'Nothing to show here' },
     400: { img: this.SAD_CLOUD, m: 'That didn\'t make sense' },
+    401: { img: this.SAD_CLOUD, m: 'Unauthorized\nYou need to login for that!' },
+    403: { img: this.SAD_CLOUD, m: 'Forbidden\nYou can\'t do that!' },
     404: { img: this.SAD_CLOUD, m: 'Couldn\'t find that anywhere\nSure you\'re in the right place?' },
+    408: { img: this.SAD_CLOUD, m: 'Timeout\nSomething isn\' right!' },
     500: { img: this.SAD_CLOUD, m: 'Sorry, couldn\'t get that\nSomething went wrong on our side' },
+    504: { img: this.SAD_CLOUD, m: 'Gateway Timeout\nSomething went really wrong!' },
     1000: { img: this.SAD_CLOUD, m: 'Something went really wrong!' },
   };
 
