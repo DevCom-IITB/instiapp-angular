8db8ca1a8ed2213a481ebcbb64682949e4d7b97d
Varun Patil <radialapps@gmail.com>
2018-08-01 23:50:32 +0530

    Disable ripple and link for user card in sandbox

--- src/app/user-card/user-card.component.ts
+++ src/app/user-card/user-card.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { IUserProfile } from '../interfaces';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-user-card',
@@ -11,7 +12,9 @@ export class UserCardComponent implements OnInit {
   @Input() profile: IUserProfile;
   @Input() role: string;
 
-  constructor() { }
+  constructor(
+    public dataService: DataService,
+  ) { }
 
   ngOnInit() {
     /* Set fallback profile pic explicitly */
