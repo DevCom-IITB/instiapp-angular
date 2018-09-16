342894254628a8dd11a2ec8f6a90a1ab0e76c704
Varun Patil <radialapps@gmail.com>
2018-09-17 01:44:31 +0530

    Remove unused environments

--- src/environments/environment.deploy.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-    production: true,
-    hmr: false,
-    host: 'https://instiapp.wncc-iitb.org/',
-    sso_host: 'https://gymkhana.iitb.ac.in/sso/oauth/authorize/',
-    sso_client_id: 'u1eFlmRWUOY5Sh4yFeqeWD71OXR8M8PYc0xUK33D',
-    service_worker_url: '/ngsw-worker.js',
-};
--- src/environments/environment.deploy2.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-    production: true,
-    hmr: false,
-    host: 'https://gymkhana.iitb.ac.in/instiapp/',
-    sso_host: 'https://gymkhana.iitb.ac.in/sso/oauth/authorize/',
-    sso_client_id: 'u1eFlmRWUOY5Sh4yFeqeWD71OXR8M8PYc0xUK33D',
-    service_worker_url: '/instiapp/ngsw-worker.js',
-};
