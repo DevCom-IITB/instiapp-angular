167701aed9c4539257be18b528e6a66ff87fafa4
Varun Patil <radialapps@gmail.com>
2019-05-30 20:18:07 +0530

    refactor(update-role): Remove use of deprecated startWith

--- src/app/update-role/update-role.component.ts
+++ src/app/update-role/update-role.component.ts
@@ -4,7 +4,7 @@ import { DataService } from '../data.service';
 import { API } from '../../api';
 import { FormControl } from '@angular/forms';
 import { Observable } from 'rxjs';
-import { startWith, debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
@@ -47,7 +47,6 @@ export class UpdateRoleComponent implements OnInit {
     }
 
     this.explorePeople = this.addForm.valueChanges.pipe(
-      startWith(null),
       debounceTime(200),
       distinctUntilChanged(),
       switchMap(val => {
