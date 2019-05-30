069fae40004b2b68c88c19c6d20dfa5a9c23b439
Varun Patil <radialapps@gmail.com>
2019-05-30 20:49:48 +0530

    big(polyfill): Pull support for IE11

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -18,40 +18,10 @@
  * BROWSER POLYFILLS
  */
 
-/** IE9, IE10 and IE11 requires all of the following polyfills. **/
-import 'core-js/es/symbol';
-import 'core-js/es/object';
-import 'core-js/es/function';
-import 'core-js/es/parse-int';
-import 'core-js/es/parse-float';
-import 'core-js/es/number';
-import 'core-js/es/math';
-import 'core-js/es/string';
-import 'core-js/es/date';
-import 'core-js/es/array';
-import 'core-js/es/regexp';
-import 'core-js/es/map';
-import 'core-js/es/weak-map';
-import 'core-js/es/set';
-
 // Polyfills for ES7
 import 'core-js/es/string';
-
-// Polyfill CSS variables for IE 11
-import cssVars from 'css-vars-ponyfill';
-cssVars();
-
-/** IE10 and IE11 requires the following for NgClass support on SVG elements */
-// import 'classlist.js';  // Run `npm install --save classlist.js`.
-
-/** IE10 and IE11 requires the following for the Reflect API. */
-// import 'core-js/es6/reflect';
-
-
-/** Evergreen browsers require these. **/
-// Used for reflect-metadata in JIT. If you use AOT (and only Angular decorators), you can remove.
-import 'core-js/es/reflect';
-
+import 'core-js/es/object';
+import 'core-js/es/array';
 
 /**
  * Required to support Web Animations `@angular/platform-browser/animations`.
