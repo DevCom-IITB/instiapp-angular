057806ceafe79fdf50b77bf1e5bcdb00ae4f6edd
Varun Patil <radialapps@gmail.com>
2019-05-30 19:27:58 +0530

    refactor(calendar): Use better typing with XDate

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -7,13 +7,19 @@ import { API } from '../../api';
 import { Observable } from 'rxjs';
 import { XunkCalendarComponent } from '../xunk-calendar/xunk-calendar.component';
 
+interface XDate {
+  date: number;
+  month: number;
+  year: number;
+}
+
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
   styleUrls: ['./calendar.component.css']
 })
 export class CalendarComponent implements OnInit {
-  public selDate = { date: 1, month: 1, year: 1 };
+  public selDate: XDate = { date: 1, month: 1, year: 1 };
   public events: IEvent[];
   public selectedEvent: IEvent;
   public error: number;
@@ -50,9 +56,9 @@ export class CalendarComponent implements OnInit {
   }
 
   /** Get events for the date */
-  GetDateEvents(date: any): IEvent[] {
+  GetDateEvents(date: XDate): IEvent[] {
     const selEvents: IEvent[] = [] as IEvent[];
-    const cDate: Date = this.dateToDate(date);
+    const cDate: Date = this.xdateToDate(date);
 
     /* Add events for the date */
     for (const event of this.events) {
@@ -68,7 +74,7 @@ export class CalendarComponent implements OnInit {
   }
 
   /** Open the first event on date change */
-  dateChanged(date: any) {
+  dateChanged(date: XDate) {
     if (this.dataService.isMobile()) { return; }
     const selEvents = this.GetDateEvents(date);
     if (selEvents.length > 0) {
@@ -77,20 +83,17 @@ export class CalendarComponent implements OnInit {
   }
 
   /** Gets human readable human count */
-  GetCount(date: any): string {
+  GetCount(date: XDate): string {
     /* Return with 'today' if date is today */
-    const now = new Date();
-    if (date.date === now.getDate() &&
-        date.month === now.getMonth() &&
-        date.year === now.getFullYear()) {
-          return this.GetCountPrefix(date) + ' Today';
+    if (this.zeroTime(new Date()).valueOf() === this.xdateToDate(date).valueOf()) {
+      return this.GetCountPrefix(date) + ' Today';
     } else {
       return this.GetCountPrefix(date);
     }
   }
 
   /** Gets prefix of count */
-  GetCountPrefix(date: any): string {
+  GetCountPrefix(date: XDate): string {
     const count = this.GetDateEvents(date).length;
     if (count === 0) {
       return 'No Events';
@@ -102,7 +105,7 @@ export class CalendarComponent implements OnInit {
   }
 
   /** Convert a custom date object to a Date object */
-  dateToDate(date: any): Date {
+  xdateToDate(date: XDate): Date {
     return this.zeroTime(new Date(date.year, date.month, date.date));
   }
 
@@ -148,7 +151,7 @@ export class CalendarComponent implements OnInit {
   }
 
   /** Update events to main directory */
-  updateEvents(events: IEvent[], monthObj: any) {
+  updateEvents(events: IEvent[], monthObj: XDate) {
     /* Mark that we have this month now */
     const monthstring = JSON.stringify(this.resetMonth(monthObj));
     this.hasEventsMonths.push(monthstring);
@@ -171,7 +174,7 @@ export class CalendarComponent implements OnInit {
   }
 
   /** Get a new object with Date of month object to 1 */
-  resetMonth(monthObj: any): any {
+  resetMonth(monthObj: XDate): XDate {
     /** Set date to 1 */
     const monthStart = {...monthObj};
     monthStart.date = 1;
@@ -179,11 +182,11 @@ export class CalendarComponent implements OnInit {
   }
 
   /** Get events for month with date */
-  fetchEventsForMonth(monthObj: any): Observable<IEnumContainer> {
+  fetchEventsForMonth(monthObj: XDate): Observable<IEnumContainer> {
     /* Get initial and final date objects */
     const monthStart = this.resetMonth(monthObj);
-    const initial = this.dateToDate(monthStart);
-    const final = this.dateToDate(monthStart);
+    const initial = this.xdateToDate(monthStart);
+    const final = this.xdateToDate(monthStart);
     initial.setDate(initial.getDate() - 1);
     final.setMonth(final.getMonth() + 1);
 
