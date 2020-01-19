6f64e40923a0c3f8e36afad85c4cccd502e58b02
Varun Patil <radialapps@gmail.com>
2020-01-19 14:22:21 +0530

    Add www prefix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   hmr: false,
-  host: 'https://insti.app/',
+  host: 'https://www.insti.app/',
   sso_host: 'https://gymkhana.iitb.ac.in/sso/oauth/authorize/',
   sso_client_id: 'vR1pU7wXWyve1rUkg0fMS6StL1Kr6paoSmRIiLXJ',
   service_worker_url: '/ngsw-worker.js',
