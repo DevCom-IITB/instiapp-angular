2a0f774b06deade00ee5d6f0c8a2a9357a007355
DheGit <bankerdheer@gmail.com>
2022-06-14 00:13:36 +0530

    Update dummy data-loading code in DiscussionsComponent
    A function now populates the groups variable which would otherwise be populated by an API call in a completely made component.

--- src/app/page/discussions/discussions.component.ts
+++ src/app/page/discussions/discussions.component.ts
@@ -12,17 +12,40 @@ export class DiscussionsComponent implements OnInit {
   constructor() { }
 
   ngOnInit(){
-    this.user[0].name='Insight';
-    this.groups[0].id = '1';
-    this.groups[0].title='Insight Discussion Forum';
-    this.groups[0].body='Followers - 3000';
-    this.groups[0].created_by = this.user[0];
+    this.populateDummyGroups();
+    // this.user[0].name='Insight';
+    // this.groups[0].id = '1';
+    // this.groups[0].title='Insight Discussion Forum';
+    // this.groups[0].body='Followers - 3000';
+    // this.groups[0].created_by = this.user[0];
 
-    this.user[1].name='DevCom';
-    this.groups[1].id = '2';
-    this.groups[1].title='Devcom Discussion Forum';
-    this.groups[1].body='Followers - 4000';
-    this.groups[1].created_by = this.user[1];
+    // this.user[1].name='DevCom';
+    // this.groups[1].id = '2';
+    // this.groups[1].title='Devcom Discussion Forum';
+    // this.groups[1].body='Followers - 4000';
+    // this.groups[1].created_by = this.user[1];
+  }
+
+  populateDummyGroups(){
+    const dummy_creator = {
+      name: "DevCom_Admin",
+    } as IUserProfile;
+
+    const group1: IGroup = {
+      id:'1',
+      title:'Insight Discussion Forum',
+      body: '4000 Following',
+      created_by:dummy_creator
+    }
+    const group2: IGroup = {
+      id:'2',
+      title:'DevCom Roxx',
+      body: '7b Following',
+      created_by:dummy_creator
+    }
+
+    this.groups.push(group1);
+    this.groups.push(group2);
   }
 
   getBodies() {
