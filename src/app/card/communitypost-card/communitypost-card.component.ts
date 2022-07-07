820741314ee9016edf5adea8078a07c3ae1e34ae
DheGit <bankerdheer@gmail.com>
2022-07-07 19:22:18 +0530

    Add thread rank indicator lines
    Thread rank indicator lines are now shown. Also, for communitypost-cards with rank greater than 1, the name and the profile pic are offset by 38px to align with the thread rank indicator lines.

--- src/app/card/communitypost-card/communitypost-card.component.ts
+++ src/app/card/communitypost-card/communitypost-card.component.ts
@@ -19,6 +19,7 @@ export class CommunityPostCardComponent implements OnInit {
 
   public printable_date: String;
   public num_reactions: number = 0;
+  public pic_name_offset: number;
 
   constructor() {
     this.show_comment_thread = false; // might make the erroneous behaviour of always keeping that at false
@@ -31,9 +32,15 @@ export class CommunityPostCardComponent implements OnInit {
       day: 'numeric'
     })
 
+    if(this.post.reaction_count == null) this.post.reaction_count = [0,0,0,0,0]; // Maybe a different default is preferred?
     this.num_reactions += this.post.reaction_count.reduce((a,b)=>a+b,0)
 
-
+    if (this.post.thread_rank > 1){
+      this.pic_name_offset = -38;
+    } else{
+      this.pic_name_offset = 0;
+    }
+    console.log(`pic_name_offset set to ${this.pic_name_offset}`);
   }
 
 }
