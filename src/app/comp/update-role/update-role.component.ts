0db213d3f857aa1999dbe7b4b3b284cf5b611baf
Palash Mittal <pmittal00@gmail.com>
2022-08-30 15:00:50 +0530

    feat(update-role): add AppP and ModC permissions

--- src/app/comp/update-role/update-role.component.ts
+++ src/app/comp/update-role/update-role.component.ts
@@ -1,47 +1,47 @@
-import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
-import { IBodyRole, IUserProfile } from '../../interfaces';
-import { DataService } from '../../data.service';
-import { API } from '../../../api';
-import { Observable } from 'rxjs';
-import { MatSnackBar } from '@angular/material/snack-bar';
+import { Component, OnInit, Input, EventEmitter, Output } from "@angular/core";
+import { IBodyRole, IUserProfile } from "../../interfaces";
+import { DataService } from "../../data.service";
+import { API } from "../../../api";
+import { Observable } from "rxjs";
+import { MatSnackBar } from "@angular/material/snack-bar";
 
 @Component({
-  selector: 'app-update-role',
-  templateUrl: './update-role.component.html',
-  styleUrls: ['./update-role.component.css']
+  selector: "app-update-role",
+  templateUrl: "./update-role.component.html",
+  styleUrls: ["./update-role.component.css"],
 })
 export class UpdateRoleComponent implements OnInit {
-
   @Input() minrole: IBodyRole;
   @Output() public doneUpdate = new EventEmitter<IBodyRole>();
   role: IBodyRole;
 
   possiblePermissions = [
-    {name: 'Add Event', code: 'AddE'},
-    {name: 'Edit Event', code: 'UpdE'},
-    {name: 'Delete Event', code: 'DelE'},
-    {name: 'Update Body', code: 'UpdB'},
-    {name: 'Modify Roles', code: 'Role'},
-    {name: 'Verify Achievements', code: 'VerA'},
+    { name: "Add Event", code: "AddE" },
+    { name: "Edit Event", code: "UpdE" },
+    { name: "Delete Event", code: "DelE" },
+    { name: "Update Body", code: "UpdB" },
+    { name: "Modify Roles", code: "Role" },
+    { name: "Verify Achievements", code: "VerA" },
+    { name: "Moderate Post", code: "AppP" },
+    { name: "Moderate Comment", code: "ModC" },
   ];
 
-  constructor(
-    public dataService: DataService,
-    public snackBar: MatSnackBar,
-  ) {}
+  constructor(public dataService: DataService, public snackBar: MatSnackBar) {}
 
   ngOnInit() {
     if (this.minrole.editing) {
       /* Modifying an existing role */
-      this.dataService.FireGET<IBodyRole>(API.Role, { uuid: this.minrole.id }).subscribe(result => {
-        this.role = result;
-      });
+      this.dataService
+        .FireGET<IBodyRole>(API.Role, { uuid: this.minrole.id })
+        .subscribe((result) => {
+          this.role = result;
+        });
     } else {
       /* Making a new role */
       this.role = this.minrole;
       this.role.users_detail = [];
       this.role.users = [];
-      this.role.permissions = this.possiblePermissions.map(p => p.code);
+      this.role.permissions = this.possiblePermissions.map((p) => p.code);
       this.role.inheritable = true;
       this.role.official_post = true;
     }
@@ -81,31 +81,42 @@ export class UpdateRoleComponent implements OnInit {
   submit() {
     let obs: Observable<IBodyRole>;
 
-    if (this.minrole.id && this.minrole.id !== '') {
-      obs = this.dataService.FirePUT<IBodyRole>(API.Role, this.role, { uuid: this.role.id });
+    if (this.minrole.id && this.minrole.id !== "") {
+      obs = this.dataService.FirePUT<IBodyRole>(API.Role, this.role, {
+        uuid: this.role.id,
+      });
     } else {
       obs = this.dataService.FirePOST<IBodyRole>(API.Roles, this.role);
     }
 
     /* Make the request */
-    obs.subscribe(result => {
-      this.snackBar.open('Role Updated', 'Dismiss', { duration: 2000 });
-      this.doneUpdate.emit(result);
-    }, () => {
-      this.snackBar.open('Update Failed', 'Dismiss', { duration: 2000 });
-    });
+    obs.subscribe(
+      (result) => {
+        this.snackBar.open("Role Updated", "Dismiss", { duration: 2000 });
+        this.doneUpdate.emit(result);
+      },
+      () => {
+        this.snackBar.open("Update Failed", "Dismiss", { duration: 2000 });
+      }
+    );
   }
 
   /* Delete the role */
   delete() {
-    if (confirm('Delete this role? This action is irreversible')) {
-      this.dataService.FireDELETE(API.Role, { uuid: this.role.id }).subscribe(() => {
-        this.snackBar.open('Role Deleted', 'Dismiss', { duration: 2000 });
-        (this.role as any).deleted = true;
-        this.doneUpdate.emit(this.role);
-      }, () => {
-        this.snackBar.open('Deletion Failed! Note that you may not delete roles held by people in the past.', 'Dismiss');
-      });
+    if (confirm("Delete this role? This action is irreversible")) {
+      this.dataService.FireDELETE(API.Role, { uuid: this.role.id }).subscribe(
+        () => {
+          this.snackBar.open("Role Deleted", "Dismiss", { duration: 2000 });
+          (this.role as any).deleted = true;
+          this.doneUpdate.emit(this.role);
+        },
+        () => {
+          this.snackBar.open(
+            "Deletion Failed! Note that you may not delete roles held by people in the past.",
+            "Dismiss"
+          );
+        }
+      );
     }
   }
 
