076745df46517630d63ad67cf21be83169ba811a
Palash Mittal <pmittal00@gmail.com>
2022-09-04 16:34:02 +0530

    feat(communtiy): load IDF by default

--- src/app/page/discussions/discussions.component.ts
+++ src/app/page/discussions/discussions.component.ts
@@ -1,18 +1,16 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { ICommunity, IUserProfile } from '../../interfaces';
-import { DataService } from '../../data.service';
-import { Router } from '@angular/router';
-import { API } from '../../../api';
+import { Component, OnInit, Input } from "@angular/core";
+import { ICommunity, IUserProfile } from "../../interfaces";
+import { DataService } from "../../data.service";
+import { Router } from "@angular/router";
+import { API } from "../../../api";
 
-const TITLE = 'Discussions';
+const TITLE = "Discussions";
 
 @Component({
-  selector: 'app-insight-disscussion-forum',
-  templateUrl: './discussions.component.html',
-  styleUrls: ['./discussions.component.css']
+  selector: "app-insight-disscussion-forum",
+  templateUrl: "./discussions.component.html",
+  styleUrls: ["./discussions.component.css"],
 })
-
-
 export class DiscussionsComponent implements OnInit {
   @Input() public groups: ICommunity[];
   @Input() public allgroups: ICommunity[];
@@ -20,36 +18,33 @@ export class DiscussionsComponent implements OnInit {
   public selected_group: ICommunity;
   public error: number;
 
-
-  constructor(
-    public dataService: DataService,
-    public router: Router,
-
-  ) { }
-
+  constructor(public dataService: DataService, public router: Router) {}
 
   ngOnInit() {
-
     this.dataService.setTitle(TITLE);
 
-    this.dataService.FireGET<ICommunity[]>(API.Communities).subscribe(result => {
-      this.groups = result;
-      this.allgroups = result;
-    }, (e) => {
-      this.error = e.status;
-    });
+    this.dataService.FireGET<ICommunity[]>(API.Communities).subscribe(
+      (result) => {
+        if (result.length > 0) {
+          //TODO: Remove this if more communities
+          // console.log(result[0]);
+          this.router.navigate(["group-feed", result[0].str_id]);
+        }
 
-
+        this.groups = result;
+        this.allgroups = result;
+      },
+      (e) => {
+        this.error = e.status;
+      }
+    );
   }
-
-
 
   getBodies() {
     return this.groups;
   }
 
   over(group: ICommunity) {
-
     // adding route
     if (this.dataService.isMobile()) {
       // this.router.navigate(['group', group.id]);
@@ -57,5 +52,4 @@ export class DiscussionsComponent implements OnInit {
       this.selected_group = group;
     }
   }
-
 }
