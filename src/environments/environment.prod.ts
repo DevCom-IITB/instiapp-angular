6ca6699743f0b9618d6fca7e23a3c4265600c9e4
Palash Mittal <pmittal00@gmail.com>
2022-10-07 01:31:48 +0530

    fix(prod): revert to prod env and proxy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,8 +3,9 @@ export const environment = {
   hmr: false,
   host: 'https://www.insti.app/',
   sso_host: 'https://gymkhana.iitb.ac.in/sso/oauth/authorize/',
-  sso_client_id: 'gxzx6u7aw6wco6SJBioORPwum5Sug7OrIPrm8r2W',
+  sso_client_id: 'vR1pU7wXWyve1rUkg0fMS6StL1Kr6paoSmRIiLXJ',
   service_worker_url: '/ngsw-worker.js',
-  VAPID_PUBLIC_KEY: 'BIH7RBzSBVprQy4-6uaUQZOp5TmrzbpCYKA2COp02jRdS1ihX2qZ3sB0SJG4_pr6G2Q2GSCfGtK8kMax19b0mz0',
+  VAPID_PUBLIC_KEY:
+    'BIH7RBzSBVprQy4-6uaUQZOp5TmrzbpCYKA2COp02jRdS1ihX2qZ3sB0SJG4_pr6G2Q2GSCfGtK8kMax19b0mz0',
   VERSION: require('../../package.json').version,
 };
