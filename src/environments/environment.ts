8195ed7c60d19d95b07c21e295b371205983fd50
Krishna-Baldwa <krishnabaldwa1101.com>
2023-05-12 00:35:17 +0530

    roles api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,11 +6,17 @@
 export const environment = {
   production: false,
   hmr: true,
-  host: 'http://localhost:4200/',
+  host: 'https://gymkhana.iitb.ac.in/instiapp/',
   sso_host: 'https://gymkhana.iitb.ac.in/sso/oauth/authorize/',
   sso_client_id: 'vR1pU7wXWyve1rUkg0fMS6StL1Kr6paoSmRIiLXJ',
   service_worker_url: '/ngsw-worker.js',
   VAPID_PUBLIC_KEY:
     'BIH7RBzSBVprQy4-6uaUQZOp5TmrzbpCYKA2COp02jRdS1ihX2qZ3sB0SJG4_pr6G2Q2GSCfGtK8kMax19b0mz0',
   VERSION: require('../../package.json').version,
+  api: {
+    "target": "https://gymkhana.iitb.ac.in/instiapp/",
+    "secure": true,
+    "changeOrigin": true,
+    "logLevel": "info"
+  }
 };
