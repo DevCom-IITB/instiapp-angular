11ff1104710a76edab4b08b8a3efc59f68acd040
VIBR0X <44766005+vedant-github@users.noreply.github.com>
2023-06-11 20:14:11 +0530

    remove proxy.config.json

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   hmr: false,
+  apiUrl:'https://gymkhana.iitb.ac.in/instiapp/',
   host: 'https://www.insti.app/',
   sso_host: 'https://gymkhana.iitb.ac.in/sso/oauth/authorize/',
   sso_client_id: 'vR1pU7wXWyve1rUkg0fMS6StL1Kr6paoSmRIiLXJ',
