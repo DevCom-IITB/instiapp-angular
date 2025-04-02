1865067a0a2920e6c264e599cc0b1563d4678383
Arnav Bhate <bhatearnav@gmail.com>
2025-04-02 23:58:46 +0530

    quick-links: updated outdated links

--- src/app/page/quick-links/quick-links.component.ts
+++ src/app/page/quick-links/quick-links.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { DataService } from '../../data.service';
+import { Component, OnInit } from "@angular/core";
+import { DataService } from "../../data.service";
 
 interface LinkContainer {
   title: string;
@@ -13,68 +13,131 @@ interface Link {
 }
 
 @Component({
-  selector: 'app-quick-links',
-  templateUrl: './quick-links.component.html',
-  styleUrls: ['./quick-links.component.css']
+  selector: "app-quick-links",
+  templateUrl: "./quick-links.component.html",
+  styleUrls: ["./quick-links.component.css"],
 })
 export class QuickLinksComponent implements OnInit {
-
   public sections: LinkContainer[] = [
     {
-      title: 'DevCom',
+      title: "DevCom",
       links: [
-        { href: 'https://www.devcom-iitb.org/', icon: 'book', text: 'DevCom Website' },
-        { href: 'https://ams.iitb.ac.in', icon: 'book', text: 'AMS' },
+        {
+          href: "https://devcom.gymkhana.iitb.ac.in/",
+          icon: "book",
+          text: "DevCom Website",
+        },
       ],
     },
     {
-      title: 'Academic',
+      title: "Academic",
       links: [
-        { href: 'https://asc.iitb.ac.in', icon: 'book', text: 'ASC' },
-        { href: 'https://portal.iitb.ac.in/asc', icon: 'book', text: 'ASC - External' },
-        { href: 'https://moodle.iitb.ac.in', icon: 'book', text: 'Moodle' },
-        { href: 'https://campus.placements.iitb.ac.in/auth/student/login', icon: 'school', text: 'Internship and Placement Login' },
-        { href: 'http://www.library.iitb.ac.in/', icon: 'local_library', text: 'Central Library' },
+        { href: "https://asc.iitb.ac.in", icon: "book", text: "ASC" },
+        {
+          href: "https://portal.iitb.ac.in/asc",
+          icon: "book",
+          text: "ASC - External",
+        },
+        { href: "https://moodle.iitb.ac.in", icon: "book", text: "Moodle" },
+        {
+          href: "https://campus.placements.iitb.ac.in/auth/student/login",
+          icon: "school",
+          text: "Internship and Placement Login",
+        },
+        {
+          href: "http://www.library.iitb.ac.in/",
+          icon: "local_library",
+          text: "Central Library",
+        },
       ],
     },
     {
-      title: 'Calendar',
+      title: "Calendar",
       links: [
-        { href: 'http://www.iitb.ac.in/newacadhome/toacadcalender.jsp', icon: 'today', text: 'Academic Calendar' },
-        { href: 'http://www.iitb.ac.in/newacadhome/timetable.jsp', icon: 'access_alarm', text: 'Academic Timetable' },
-        { href: 'http://www.iitb.ac.in/en/about-iit-bombay/iit-bombay-holidays-list', icon: 'whatshot', text: 'Holidays List' },
-        { href: 'http://www.iitb.ac.in/newacadhome/circular.jsp', icon: 'info', text: 'Circulars' },
-        { href: 'https://portal.iitb.ac.in/asc/Courses', icon: 'school', text: 'Course List' },
+        {
+          href: "http://www.iitb.ac.in/newacadhome/toacadcalender.jsp",
+          icon: "today",
+          text: "Academic Calendar",
+        },
+        {
+          href: "http://www.iitb.ac.in/newacadhome/timetable.jsp",
+          icon: "access_alarm",
+          text: "Academic Timetable",
+        },
+        {
+          href: "https://www.iitb.ac.in/holidays-list",
+          icon: "whatshot",
+          text: "Holidays List",
+        },
+        {
+          href: "http://www.iitb.ac.in/newacadhome/circular.jsp",
+          icon: "info",
+          text: "Circulars",
+        },
+        {
+          href: "https://portal.iitb.ac.in/asc/Courses",
+          icon: "school",
+          text: "Course List",
+        },
       ],
     },
     {
-      title: 'Services',
+      title: "Services",
       links: [
-        { href: 'https://sso.iitb.ac.in/', icon: 'account_box', text: 'SSO Login' },
-        { href: 'https://webmail.iitb.ac.in', icon: 'email', text: 'WebMail' },
-        { href: 'https://camp.iitb.ac.in/', icon: 'account_box', text: 'CAMP' },
-        { href: 'https://home.iitb.ac.in/', icon: 'cloud', text: 'BigHome Cloud' },
-        { href: 'ftp://ftp.iitb.ac.in/', icon: 'folder_open', text: 'FTP Server' },
+        {
+          href: "https://sso.iitb.ac.in/",
+          icon: "account_box",
+          text: "SSO Login",
+        },
+        { href: "https://webmail.iitb.ac.in", icon: "email", text: "WebMail" },
+        { href: "https://camp.iitb.ac.in/", icon: "account_box", text: "CAMP" },
+        {
+          href: "https://home.iitb.ac.in/",
+          icon: "cloud",
+          text: "BigHome Cloud",
+        },
+        {
+          href: "ftp://ftp.iitb.ac.in/",
+          icon: "folder_open",
+          text: "FTP Server",
+        },
       ],
     },
     {
-      title: 'Miscellaneous',
+      title: "Miscellaneous",
       links: [
-        { href: 'https://access.iitb.ac.in/', icon: 'phone', text: 'CC Access Portal' },
-        { href: 'https://portal.iitb.ac.in/TelephoneDirectory/', icon: 'phone', text: 'Intercom Extensions' },
-        { href: 'http://www.iitb.ac.in/hospital/', icon: 'local_hospital', text: 'Hospital' },
-        { href: 'https://www.cc.iitb.ac.in/page/configurevpn', icon: 'vpn_lock', text: 'VPN Guide' },
-        { href: 'https://www.iitb-bandhu.com/', icon: 'emoji_people', text: 'Bandhu IITB' },
-      ]
-    }
+        {
+          href: "https://access.iitb.ac.in/",
+          icon: "phone",
+          text: "CC Access Portal",
+        },
+        {
+          href: "https://portal.iitb.ac.in/TelephoneDirectory/",
+          icon: "phone",
+          text: "Intercom Extensions",
+        },
+        {
+          href: "http://www.iitb.ac.in/hospital/",
+          icon: "local_hospital",
+          text: "Hospital",
+        },
+        {
+          href: "https://www.cc.iitb.ac.in/page/configurevpn",
+          icon: "vpn_lock",
+          text: "VPN Guide",
+        },
+        {
+          href: "https://www.iitb-bandhu.com/",
+          icon: "emoji_people",
+          text: "Bandhu IITB",
+        },
+      ],
+    },
   ];
 
-  constructor(
-    public dataService: DataService,
-  ) { }
+  constructor(public dataService: DataService) {}
 
   ngOnInit() {
-    this.dataService.setTitle('Quick Links');
+    this.dataService.setTitle("Quick Links");
   }
-
 }
